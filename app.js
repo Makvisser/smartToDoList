175cb17d20abfdedab22d101911098281658455e
Makvisser <84993129+Makvisser@users.noreply.github.com>
2025-11-19 13:35:15 +0000

    feat: create add form

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 const tasks = [
     { id: 1, text: "Доделать стилизацию", completed: false },
     { id: 2, text: "сделать стилизацию формы добавления и еще немножко", completed: false },
-    { id: 3, text: "Add to function add", completed: false },
-    { id: 4, text: "to do commit", completed: false },
-    { id: 5, text: "add to function edit", completed: false },
-    { id: 6, text: "add to function delete", completed: false },
-    { id: 7, text: "add to filter", completed: false },
+    { id: 3, text: "Add to function add", completed: true },
+    { id: 4, text: "to do commit", completed: true },
+    { id: 5, text: "add to function edit", completed: true },
+    { id: 6, text: "add to function delete", completed: true },
+    { id: 7, text: "add to filter", completed: true },
     { id: 8, text: "Доделать стилизацию", completed: false },
     { id: 9, text: "сделать стилизацию формы добавлени", completed: false },
-    { id: 10, text: "Add to function add", completed: false },
-    { id: 11, text: "to do commit", completed: false },
-    { id: 12, text: "add to function edit", completed: false },
-    { id: 13, text: "add to function delete", completed: false },
-    { id: 14, text: "add to filter", completed: false },
-    { id: 15, text: "Add to function add", completed: false },
-    { id: 16, text: "to do commit", completed: false },
-    { id: 17, text: "add to function edit", completed: false },
-    { id: 18, text: "add to function delete", completed: false },
-    { id: 19, text: "add to filter", completed: false }
+    { id: 10, text: "Add to function add", completed: true },
+    { id: 11, text: "to do commit", completed: true },
+    { id: 12, text: "add to function edit", completed: true },
+    { id: 13, text: "add to function delete", completed: true },
+    { id: 14, text: "add to filter", completed: true },
+    { id: 15, text: "Add to function add", completed: true },
+    { id: 16, text: "to do commit", completed: true },
+    { id: 17, text: "add to function edit", completed: true },
+    { id: 18, text: "add to function delete", completed: true },
+    { id: 19, text: "add to filter", completed: true }
 ]
 
 function renderTasks(taskToRender = tasks) {
@@ -47,16 +47,52 @@ function renderTasks(taskToRender = tasks) {
 // Запускаем при загрузке страницы
 document.addEventListener('DOMContentLoaded', renderTasks);
 
+function addNewTask(text) {
+    const newTask = {
+        id: Date.now(),
+        text: text,
+        completed: false
+    };
 
+    tasks.push(newTask);
+    renderTasks();
+}
 const addBtn = document.querySelector('.add-btn');
-const addTaskForm = document.querySelector('.addTask');
 
 addBtn.addEventListener('click', function () {
     addTaskForm.hidden = false;
+    const backdrop = document.createElement('div');
+    backdrop.className = 'addTask-backdrop';
+    document.body.appendChild(backdrop);
+});
+
+const addTaskForm = document.querySelector('.addTask');
+const taskInput = addTaskForm.querySelector('input[type="text"]');
+const cancelBtn = addTaskForm.querySelector('button[type="button"]');
+const submitBtn = addTaskForm.querySelector('button[type="submit"]');
+
+submitBtn.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    addNewTask(taskInput.value);
+    taskInput.value = '';
+    closeAddTaskForm();
+})
+
+function closeAddTaskForm() {
+    addTaskForm.hidden = true;
+    const backdrop = document.querySelector('.addTask-backdrop');
+    if (backdrop) {
+        backdrop.remove();
+    }
+}
+
+cancelBtn.addEventListener('click', function () {
+    taskInput.value = '';
+    closeAddTaskForm();
 });
 
 const searchInput = document.querySelector('.search-input');
-// const searchBtn = document.querySelector('search-btn');
 
 searchInput.addEventListener('input', function (event) {
     const searchValue = event.target.value.toLowerCase();
@@ -77,6 +113,28 @@ searchInput.addEventListener('input', function (event) {
         renderTasks(foundItems);
     } else {
         console.error('foundItems is not an array:', foundItems);
-        renderTasks(tasks); // fallback на все задачи
+        renderTasks(tasks);
     }
 });
+
+const filterSelect = document.querySelector('.filter-select');
+filterSelect.addEventListener('change', function (event) {
+    const filterValue = event.target.value;
+    console.log(filterValue);
+
+    let filterItems;
+    if (filterValue === "all") {
+        filterItems = tasks;
+    };
+    if (filterValue === "complete") {
+        filterItems = tasks.filter(item =>
+            item.completed === true);
+    }
+    if (filterValue === "incomplete") {
+        filterItems = tasks.filter(item =>
+            item.completed === false);
+    }
+
+    renderTasks(filterItems);
+});
+
